1082e63db4aa40f7810b5a9bc320ac6cc6dbf711
kim <kim@kimui-MacBookPro.local>
2022-05-26 10:04:07 +0900

    feat: Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    token: getAuthFromCookie() || "", // 쿠키에 저장된 토큰 값이거나 빈값
-    username: "",
+    token: getAuthFromCookie() || "", // 쿠키에 저장된 난독화된 토큰 값이거나 빈값
+    username: "", // 사용자 이름
   },
+
   // watch
   getters: {
     isLogin(state) {
       return state.username !== "";
     },
   },
+
   // setState
   mutations: {
     setToken(state, payload) {
