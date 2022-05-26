6c72ebbf8290516269c31bc2396e24e6e75e0f52
kim <kim@kimui-MacBookPro.local>
2022-05-26 10:07:11 +0900

    feat: cookie settings

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,7 +1,9 @@
+// 사용자 토큰 쿠키에 저장
 export const saveAuthToCookie = (value) => {
   document.cookie = `user_auth = ${value}`;
 };
 
+// 쿠키에 저장된 사용자 토큰 난독화 후 가져오기
 export const getAuthFromCookie = () => {
   return document.cookie.replace(
     /(?:(?:^|.*;\s*)user_auth\s*=|s*([^;]*).*$)|^.*$/,
